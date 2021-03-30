34718d81091c8273834a80f853bac72e452ad30f
gnf22 <gnferreira2000@gmail.com>
2021-03-30 17:20:34 -0300

    add: AppError

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,8 +1,11 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
 
 import '@shared/infra/typeorm';
 
 import '@shared/container';
+import AppError from '@shared/errors/AppError';
+
 import { router } from './routes';
 
 const app = express();
@@ -12,5 +15,20 @@ const port = process.env.PORT || 3333;
 app.use(express.json());
 
 app.use(router);
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        message: err.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: 'error',
+      message: `Internal server error - ${err.message}`,
+    });
+  },
+);
 
 app.listen(port, () => console.log(`Server is running on port:${port}`));
