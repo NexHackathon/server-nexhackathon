0c201c237a4d3445eabe0194dcf6c011f4baec04
gnf22 <gnferreira2000@gmail.com>
2021-03-30 01:15:45 -0300

    add: user typeorm entity

---
+++ src/modules/users/infra/typeorm/entities/User.ts
@@ -0,0 +1,66 @@
+import {
+  Column,
+  CreateDateColumn,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { v4 as uuidV4 } from 'uuid';
+
+export class User {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  school: string;
+
+  @Column()
+  date_of_birth: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  isAdmin: boolean;
+
+  @Column()
+  profile_image: string;
+
+  @Column()
+  headline: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  points: number;
+
+  @Column()
+  linkedin: string;
+
+  @Column()
+  github: string;
+
+  @Column()
+  instagram: string;
+
+  @Column()
+  team_id: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4();
+    }
+  }
+}
