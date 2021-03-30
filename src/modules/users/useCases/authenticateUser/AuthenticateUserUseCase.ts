7728748589e839b1da78ee045ebfafe3f2c87ad8
gnf22 <gnferreira2000@gmail.com>
2021-03-30 18:34:22 -0300

    add: authenticase user use case

---
+++ src/modules/users/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,64 @@
+import { sign } from 'jsonwebtoken';
+import { inject, injectable } from 'tsyringe';
+
+import authConfig from '@config/auth';
+import { IHashProvider } from '@modules/users/providers/HashProvider/models/IHashProvider';
+import { IUsersRepository } from '@modules/users/repositories/IUsersRepository';
+import AppError from '@shared/errors/AppError';
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+  };
+  token: string;
+}
+
+@injectable()
+export class AuthenticateUserUseCase {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+    @inject('HashProvider')
+    private hashProvider: IHashProvider,
+  ) {}
+
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('Email or password incorrect!');
+    }
+
+    const passwordMatch = await this.hashProvider.compareHash(
+      password,
+      user.password,
+    );
+
+    if (!passwordMatch) {
+      throw new AppError('Email or password incorrect!');
+    }
+
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
+      subject: user.id,
+      expiresIn,
+    });
+
+    const tokenReturn: IResponse = {
+      token,
+      user: {
+        name: user.name,
+        email: user.email,
+      },
+    };
+
+    return tokenReturn;
+  }
+}
