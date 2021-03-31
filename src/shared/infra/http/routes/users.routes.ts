2f940b39b3a3c5d97f0179883579e00701cbaf86
gnf22 <gnferreira2000@gmail.com>
2021-03-31 14:39:00 -0300

    add: GET/users/connections

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -1,9 +1,20 @@
 import { Router } from 'express';
 
 import { CreateUserController } from '@modules/users/useCases/createUser/CreateUserController';
+import { RegisteredUsersController } from '@modules/users/useCases/registeredUsers/RegisteredUsersController';
+
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 
 export const usersRoutes = Router();
 
 const createUserController = new CreateUserController();
 
+const registeredUsersController = new RegisteredUsersController();
+
 usersRoutes.post('/', createUserController.handle);
+
+usersRoutes.get(
+  '/connections',
+  ensureAuthenticated,
+  registeredUsersController.handle,
+);
