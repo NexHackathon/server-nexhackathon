524794a910bc90cb4ca0c8797b7ed50e0178421d
gnf22 <gnferreira2000@gmail.com>
2021-03-31 16:18:22 -0300

    add: disk storage tsyringe config

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,20 +1,43 @@
 import crypto from 'crypto';
-import multer from 'multer';
-import { resolve } from 'path';
+import multer, { StorageEngine } from 'multer';
+import path from 'path';
+
+const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
+
+interface IUploadConfig {
+  driver: 'disk';
+  tmpFolder: string;
+  uploadFolder: string;
+
+  multer: {
+    storage: StorageEngine;
+  };
+
+  config: {
+    // eslint-disable-next-line @typescript-eslint/ban-types
+    disk: {};
+  };
+}
 
 export default {
-  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-  upload(folder: string) {
-    return {
-      storage: multer.diskStorage({
-        destination: resolve(__dirname, '..', '..', folder),
-        filename: (request, file, callback) => {
-          const fileHash = crypto.randomBytes(16).toString('hex');
-          const fileName = `${fileHash}-${file.originalname}`;
+  driver: 'disk',
+  tmpFolder,
+  uploadFolder: path.resolve(tmpFolder, 'uploads'),
 
-          return callback(null, fileName);
-        },
-      }),
-    };
+  multer: {
+    storage: multer.diskStorage({
+      destination: tmpFolder,
+      filename(request, file, callback) {
+        const fileHash = crypto.randomBytes(10).toString('hex');
+
+        const fileName = `${fileHash}-${file.originalname}`;
+
+        return callback(null, fileName);
+      },
+    }),
   },
-};
+
+  config: {
+    disk: {},
+  },
+} as IUploadConfig;
