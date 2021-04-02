d6d4975aa4104285e0b8c898b8608a6814ae2796
gnf22 <gnferreira2000@gmail.com>
2021-04-02 02:38:00 -0300

    add: CreateSkill use case

---
+++ src/modules/skills/useCases/createSkills/CreateSkillsUseCase.ts
@@ -0,0 +1,30 @@
+import { inject, injectable } from 'tsyringe';
+
+import { Skill } from '@modules/skills/infra/typeorm/entities/Skill';
+import {
+  ICreateSkillDTO,
+  ISkillsRepository,
+} from '@modules/skills/repositories/ISkillsRepository';
+import AppError from '@shared/errors/AppError';
+
+@injectable()
+export class CreateSkillsUseCase {
+  constructor(
+    @inject('SkillsRepository')
+    private skillsRepository: ISkillsRepository,
+  ) {}
+
+  async execute({ name }: ICreateSkillDTO): Promise<Skill> {
+    const skillsExists = await this.skillsRepository.findByName(name);
+
+    if (skillsExists) {
+      throw new AppError('Skill already exists!');
+    }
+
+    const skill = await this.skillsRepository.create({
+      name,
+    });
+
+    return skill;
+  }
+}
