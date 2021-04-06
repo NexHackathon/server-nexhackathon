63c99a8147b9d169a71eab2818747dea89e705e9
gnf22 <gnferreira2000@gmail.com>
2021-04-06 19:56:39 -0300

    config: tsyringe TeamUsersTokenRepository

--- src/modules/teams/repositories/ITeamUsersTokenRepository.ts
+++ src/modules/teams/repositories/ITeamUsersTokenRepository.ts
@@ -1,7 +1,7 @@
 import { ICreateTeamUsersTokenDTO } from '../dtos/ICreateTeamUsersTokenDTO';
 import { TeamUserToken } from '../infra/typeorm/entities/TeamUserToken';
 
-export interface ITeamUsersToken {
+export interface ITeamUsersTokenRepository {
   findByToken(token: string): Promise<TeamUserToken>;
   generate(data: ICreateTeamUsersTokenDTO): Promise<TeamUserToken>;
 }
--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -6,7 +6,9 @@ import './providers';
 import { SkillsRepository } from '@modules/skills/infra/typeorm/repositories/SkillsRepository';
 import { ISkillsRepository } from '@modules/skills/repositories/ISkillsRepository';
 import { TeamsRepository } from '@modules/teams/infra/typeorm/repositories/TeamsRepository';
+import { TeamUsersTokenRepository } from '@modules/teams/infra/typeorm/repositories/TeamUsersTokenRepository';
 import { ITeamsRepository } from '@modules/teams/repositories/ITeamsRepository';
+import { ITeamUsersTokenRepository } from '@modules/teams/repositories/ITeamUsersTokenRepository';
 import { UsersRepository } from '@modules/users/infra/typeorm/repositories/UsersRepository';
 import { IUsersRepository } from '@modules/users/repositories/IUsersRepository';
 
@@ -23,4 +25,9 @@ container.registerSingleton<IUsersRepository>(
 container.registerSingleton<ISkillsRepository>(
   'SkillsRepository',
   SkillsRepository,
+);
+
+container.registerSingleton<ITeamUsersTokenRepository>(
+  'TeamUsersTokenRepository',
+  TeamUsersTokenRepository,
 );
